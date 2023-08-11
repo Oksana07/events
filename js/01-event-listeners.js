408060f512cf751d86c4cdb77ae2e5c7ca2c1340
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-08-11 11:53:51 +0300

    fix

--- js/01-event-listeners.js
+++ js/01-event-listeners.js
@@ -12,3 +12,21 @@
 const targetBtn = document.querySelector('.js-target-btn');
 const addListenerBtn = document.querySelector('.js-add-listener');
 const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+addListenerBtn.addEventListener('click', event => {
+  console.log(event);
+
+  console.log('Вішаєм слухача на цільову кнопку');
+
+  targetBtn.addEventListener('click', onTagergetBtnClick);
+});
+
+removeListenerBtn.addEventListener('click', event => {
+  console.log('Знімаю слухача події по цльовій кнопці');
+
+  targetBtn.removeEventListener('click', onTagergetBtnClick);
+});
+
+function onTagergetBtnClick(event) {
+  console.log('Клік по цільовій кнопці');
+}
--- js/02-forms.js
+++ js/02-forms.js
@@ -6,3 +6,22 @@
  */
 
 const form = document.querySelector('.js-register-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  //   console.dir(event.currentTarget.elements.subscription.value);
+
+  const formData = new FormData(event.currentTarget);
+
+  //   console.log(event.currentTarget); ссилка на форму
+
+  console.log(formData);
+
+  formData.forEach((value, name) => {
+    console.log('value', value);
+    console.log('name', name);
+  });
+}
