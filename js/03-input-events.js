9956e78df65e722cf06646449d9885c3172fabc5
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-08-11 12:51:09 +0300

    робота на уроці

--- js/03-input-events.js
+++ js/03-input-events.js
@@ -2,15 +2,29 @@
  * Патерн «Обєкт силок»
  *
  * Події
- * - focus и blur
- * - input и change
+ * - focus і blur
+ * - input і change
  * - Чекбокси і властивість checked
  */
 
-const input = document.querySelector('.js-input');
-const license = document.querySelector('.js-license');
-const nameLabel = document.querySelector('.js-button > span');
-const btn = document.querySelector('.js-button');
+// const input = document.querySelector('.js-input');
+// const license = document.querySelector('.js-license');
+// const nameLabel = document.querySelector('.js-button > span');
+// const btn = document.querySelector('.js-button');
+
+const rest = {
+  input: document.querySelector('.js-input'),
+  license: document.querySelector('.js-license'),
+  nameLabel: document.querySelector('.js-button > span'),
+  btn: document.querySelector('.js-button'),
+};
+
+// rest.input.addEventListener('focus', onInputFocus);
+// rest.input.addEventListener('blur', onInputBlur);
+// rest.input.addEventListener('input', onInputChange);
+
+rest.input.addEventListener('input', onInputChange);
+rest.license.addEventListener('change', onLicenseChange);
 
 function onInputFocus() {
   console.log('Інпут отримав фокус - подія focus');
@@ -20,6 +34,16 @@ function onInputBlur() {
   console.log('Інпут втратив фокус - подія blur');
 }
 
-function onInputChange() {}
+function onInputChange(event) {
+  console.log(event.currentTarget.value);
 
-function onLicenseChange() {}
+  rest.nameLabel.textContent = event.currentTarget.value;
+}
+
+function onLicenseChange(event) {
+  // console.log(event.currentTarget.checked);
+
+  console.log(rest.btn.disabled);
+
+  rest.btn.disabled = !event.currentTarget.checked;
+}
